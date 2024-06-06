9252bf43334e9cd37cd459d6027f8318fd001d20
haridassaiprakash <haridassai7893@gmail.com>
2024-06-06 10:34:55 +0000

    unknown label added

--- triton_server/triton-model-repository/end2end/model_repository/intent_postprocessor/1/model.py
+++ triton_server/triton-model-repository/end2end/model_repository/intent_postprocessor/1/model.py
@@ -1,15 +1,6 @@
 import numpy as np
 from transformers import AutoTokenizer
-
-# triton_python_backend_utils is available in every Triton Python model. You
-# need to use this module to create inference requests and responses. It also
-# contains some utility functions for extracting information from model_config
-# and converting Triton input/output types to numpy types.
-
-
 import triton_python_backend_utils as pb_utils
-
-
 
 class TritonPythonModel:
     """Your Python model must use the same class name. Every Python model
@@ -33,12 +24,15 @@ class TritonPythonModel:
         """
         model_name = 'ai4bharat/indic-bert'
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        labels_to_ids = {'balance_check': 0, 'cancel': 1, 'confirm': 2, 'electricity_payment': 3, 'emi_collection_full': 4, 'emi_collection_partial': 5, 'fastag_recharge': 6, 'gas_payment': 7, 'inform': 8, 'insurance_renewal': 9, 'mobile_recharge_postpaid': 10, 'mobile_recharge_prepaid': 11, 'p2p_transfer': 12, 'petrol_payment': 13, 'upi_creation': 14}
-        self.ids_to_labels = {
-            intent_id: intent_label
-            for intent_label, intent_id in labels_to_ids.items()
-        }
-        
+        self.labels_to_ids = {'balance_check': 0, 'cancel': 1, 'confirm': 2, 'electricity_payment': 3, 'emi_collection_full': 4, 'emi_collection_partial': 5, 'fastag_recharge': 6, 'gas_payment': 7, 'inform': 8, 'insurance_renewal': 9, 'mobile_recharge_postpaid': 10, 'mobile_recharge_prepaid': 11, 'p2p_transfer': 12, 'petrol_payment': 13, 'upi_creation': 14}
+        self.ids_to_labels = {intent_id: intent_label for intent_label, intent_id in self.labels_to_ids.items()}
+        self.conf_threshold = 0.75 
+
+    def softmax(self, logits):
+        """Compute softmax values for each set of scores in logits."""
+        exp_scores = np.exp(logits - np.max(logits, axis=-1, keepdims=True))
+        return exp_scores / np.sum(exp_scores, axis=-1, keepdims=True)
+
     def execute(self, requests):
         """`execute` MUST be implemented in every Python model. `execute`
         function receives a list of pb_utils.InferenceRequest as the only
@@ -61,26 +55,23 @@ class TritonPythonModel:
 
         responses = []
 
-        # Every Python backend must iterate over everyone of the requests
-        # and create a pb_utils.InferenceResponse for each of them.
-        
         for request in requests:
-
-            logits = pb_utils.get_input_tensor_by_name(request, 'logits').as_numpy().argmax(-1)
+            logits = pb_utils.get_input_tensor_by_name(request, 'logits').as_numpy()
+            probs = self.softmax(logits)
+            max_probs = np.max(probs, axis=-1)
+            preds = np.argmax(probs, axis=-1)
 
             batch_predictions = []
-            
-            for pred in logits:
-                
-                _prediction = self.ids_to_labels[pred]
+            for prob, pred in zip(max_probs, preds):
+                if prob < self.conf_threshold:
+                    _prediction = "unknown"
+                else:
+                    _prediction = self.ids_to_labels[pred]
                 batch_predictions.append(_prediction)
-            
+
             labels = np.array([l.encode('utf-8') for l in batch_predictions])
             labels_tensor = pb_utils.Tensor("labels", labels)
-            
-            inference_response = pb_utils.InferenceResponse(
-                output_tensors=[labels_tensor])
+            inference_response = pb_utils.InferenceResponse(output_tensors=[labels_tensor])
             responses.append(inference_response)
-        
+
         return responses
-    
